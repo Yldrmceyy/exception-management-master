03728cccacf4963c96b15447d0ddc84954344e2a
Ceylan Yıldırım <yldrmceylan.1@gmail.com>
2024-11-11 18:29:38 +0300

    standardized API response structure with RootEntity

--- exception-management-master/exception-management/src/main/java/com/ceylanyildirim/model/RootEntity.java
+++ exception-management-master/exception-management/src/main/java/com/ceylanyildirim/model/RootEntity.java
@@ -1,5 +1,6 @@
 package com.ceylanyildirim.model;
 
+import jakarta.persistence.criteria.Root;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,4 +14,23 @@ public class RootEntity<T> {
     private  boolean result;
     private String errorMessage;
     private  T data;
+
+    public <T>RootEntity <T> ok(T data){
+        RootEntity<T> rootEntity=new RootEntity<>();
+        rootEntity.setData(data);
+        rootEntity.setResult(true);
+        rootEntity.setErrorMessage(null);
+
+        return rootEntity;
+    }
+
+
+    public <T>RootEntity <T> error(String errorMessage){
+        RootEntity<T> rootEntity=new RootEntity<>();
+        rootEntity.setData(null);
+        rootEntity.setResult(false);
+        rootEntity.setErrorMessage(errorMessage);
+
+        return rootEntity;
+    }
 }
